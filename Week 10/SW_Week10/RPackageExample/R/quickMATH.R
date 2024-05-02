10c3882e39ff9b8875f717546be97ba6278c0351
DevFireGrasp <122560240+DevFireGrasp@users.noreply.github.com>
2024-05-01 20:02:26 -0400

    SW_Week10 HW (#63)
    * Week 10 Catchup
    
    HW for week 10, Also did missing week 3 HW
    
    * Update SW_Week3.Rmd
    
    * SW_Week11
    
    Week 11 HW

---
+++ Week 10/SW_Week10/RPackageExample/R/quickMATH.R
@@ -0,0 +1,23 @@
+
+#' Function takes one input, adds input to itself, and returns
+#' @export
+#' @param x numeric variable
+#' addSelf()
+
+
+addSelf <- function(x) {
+  #Adding the input to itself
+  return(x + x)
+}
+
+
+#' Function takes one input, multiplies input to itself, and returns
+#' @export
+#' @param x numeric variable
+#' multSelf()
+
+
+multSelf <- function(x) {
+  #Multiplying the input to itself
+  return(x * x)
+}
